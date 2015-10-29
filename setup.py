0928da4d6dbc2f539072d0eae2f725dcf97654e1
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2015-10-29 02:51:31 +0200

    Refactored setup.py: split config and requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import io
 import sys
-from setuptools import setup, find_packages
+from os.path import dirname, join
+from setuptools import setup
 
 if sys.version_info < (3, 4):
     print("Smilepack requires Python 3.4 or later.")
     sys.exit(1)
+
+
+def read(*args):
+    return io.open(join(dirname(__file__), *args)).read()
+
+
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+
+requirements = read("requirements.production.txt").splitlines(),
+test_requirements = read("requirements.testing.txt").splitlines(),
+extra_requirements = read("requirements.extra.txt").splitlines(),
 
 setup(
     name='smilepack',
@@ -17,34 +30,30 @@ setup(
     license='MIT',
     url='https://bitbucket.org/andreymal/smilepack',
     platforms=['linux', 'osx', 'bsd'],
-    packages=find_packages(),
-    install_requires=[
-        'Flask',
-        'Flask-Babel',
-        'Flask-Script',
-        'Flask-Webpack',
-        'pony',
-        'jsonschema',
-        'pytz',
-        'mysqlclient',
-        'python3-memcached',
-        'Flask-Limiter',
-        'pymemcache',
-        'Pillow',
-    ],
+    packages=['smilepack'],
+    install_requires=requirements,
     extras_require={
-        'imgur': ['Flask-Imgur'],
+        'imgur': extra_requirements,
     },
     include_package_data=True,
-    scripts=['bin/smilepack'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'smilepack=smilepack.manage.manager:run'
+        ],
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Flask',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX :: BSD',
-        'Operating System :: POSIX :: Linux',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
     ],
+    test_suite="tests",
+    tests_require=requirements + test_requirements,
 )
