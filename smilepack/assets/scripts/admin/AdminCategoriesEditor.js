6025c81d811dd332c09e18696b8f0ef9b244778b
andreymal <andriyano-31@mail.ru>
2016-01-25 20:11:25 +0300

    More stability at network errors

--- smilepack/assets/scripts/admin/AdminCategoriesEditor.js
+++ smilepack/assets/scripts/admin/AdminCategoriesEditor.js
@@ -28,8 +28,12 @@ AdminCategoriesEditor.prototype._onmove = function(options) {
         return {name: 'move', beforeId: options.smileId};  // reject
     }
 
-    var smileIds = options.container.getSmileIds(options.container.getCurrentGroupId());
+    var smileIds = this.collection.getSmileIds(this.collection.getCurrentGroupId());
+    var oldBeforeId = smileIds.indexOf(options.smileId);
+    oldBeforeId = (oldBeforeId < smileIds.length - 1) ? smileIds[oldBeforeId + 1] : null;
+
     smileIds.splice(smileIds.indexOf(options.smileId), 1);
+
     if (options.beforeId !== null) {
         smileIds.splice(smileIds.indexOf(options.beforeId), 0, options.smileId);
     } else {
@@ -46,12 +50,12 @@ AdminCategoriesEditor.prototype._onmove = function(options) {
             after: afterId,
             check_order: order
         },
+        oldBeforeId,
+        this.collection.getCurrentGroupId()
     ]);
     if (!this._orderQueueWorking) {
         this._orderQueueNext();
     }
-
-
 };
 
 
@@ -67,9 +71,22 @@ AdminCategoriesEditor.prototype._orderQueueNext = function() {
         data[0],
         {position: data[1]},
         null,
-        function(data) {
-            alert(data.error === 'Result checking failed' ? 'Не получилось переместить смайлик; возможно,\nкто-то ещё редактирует категорию помимо вас.\nПопробуйте обновить страницу.' : (data.error || data || 'fail'));
-        },
+        function(response) {
+            if (response.error === 'Result checking failed') {
+                alert('Не получилось переместить смайлик; возможно,\nкто-то ещё редактирует категорию помимо вас.\nПопробуйте обновить страницу.');
+            } else {
+                alert(response.error || response || 'fail');
+                for (var i = this._orderQueue.length - 1; i >= 0; i--) {
+                    if (!this.collection.moveSmile(this._orderQueue[i][0], this._orderQueue[i][2], this._orderQueue[i][3])) {
+                        throw new Error('Cannot rollback smiles moving :(');
+                    }
+                }
+                this._orderQueue = [];
+                if (!this.collection.moveSmile(data[0], data[2], data[3])) {
+                    throw new Error('Cannot rollback smiles moving :(');
+                }
+            }
+        }.bind(this),
         function() {
             this._orderQueueNext();
         }.bind(this)
